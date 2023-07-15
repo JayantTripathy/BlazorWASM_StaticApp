06fe55e96a8198d40f6bd56e816551a4535b8599
jayant <jayanttripathy1985@gmail.com>
2023-07-15 14:11:54 +0530

    Add project files.

---
+++ BlazorWASM_StaticApp/Extensions/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorWASM_StaticApp.Extensions
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> list)
+        {
+            var r = new Random();
+            var shuffledList =
+                list.
+                    Select(x => new { Number = r.Next(), Item = x }).
+                    OrderBy(x => x.Number).
+                    Select(x => x.Item);
+
+            return shuffledList.ToList();
+        }
+    }
+}
---
+++ BlazorWASM_StaticApp/Program.cs
@@ -0,0 +1,13 @@
+using BlazorWASM_StaticApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var baseAddress = builder.HostEnvironment.BaseAddress;
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
